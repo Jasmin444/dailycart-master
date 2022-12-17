12c492d6a14adc11dc9281de65cf6dc95068cb2c
Jasmin444 <67747476+Jasmin444@users.noreply.github.com>
2022-12-17 22:17:55 +0600

    frontend

--- data.js
+++ data.js
@@ -1,16 +1,41 @@
-import bcrypt from 'bcryptjs';
-
 const data = {
-  users: [
-    {
-      name: 'Farah',
-      email: 'farah@gmail.com',
-      password: bcrypt.hashSync('123456'),
-      isAdmin: true,
-    },
-  ],
   products: [
-    
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality Products',
+    },
+    {
+      name: 'Adidas Slim Pant',
+      slug: 'adidas-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 66,
+      countInStock: 7,
+      brand: 'Adidas',
+      rating: 4.8,
+      numReviews: 19,
+      description: 'High Quality Products',
+    },
+    {
+      name: 'Nike Slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p2.jpg',
+      price: 80,
+      countInStock: 12,
+      brand: 'Nike',
+      rating: 4.7,
+      numReviews: 11,
+      description: 'High Quality Products',
+    },
   ],
 };
 
